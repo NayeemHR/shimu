f5395d353ec760205eab56cb69986235e8f51940
Nayeem Hassan <nayeemhasan042@gmail.com>
2025-02-03 15:40:24 +0600

    add bootstrap css and js

--- functions.php
+++ functions.php
@@ -7,13 +7,15 @@
 
 function shimu_enqueue_scripts()
 {
-
+    // CSS
     wp_enqueue_style('style', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
-    wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/js/main.js', [], filemtime(get_template_directory() . '/assets/js/main.js'), true);
-
     wp_register_style('main-css', get_template_directory_uri() . '/assets/css/main.css', [], filemtime(get_template_directory() . '/assets/css/main.css'), 'all');
-
     wp_enqueue_style('main-css');
+    wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css', [], false, );
+
+    //JS
+    wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/js/main.js', [], filemtime(get_template_directory() . '/assets/js/main.js'), true);
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', ['jquery'], false, true);
 }
 
 add_action('wp_enqueue_scripts', 'shimu_enqueue_scripts');
