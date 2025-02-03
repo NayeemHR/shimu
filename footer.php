ac83136c42514a42f17d662c9560f3422cc8b8f0
Nayeem Hassan <nayeemhasan042@gmail.com>
2025-02-03 16:54:07 +0600

    get_template_part function

--- footer.php
+++ footer.php
@@ -6,6 +6,8 @@
      */
 
 ?>
+</div>
+</div>
 <?php wp_footer()?>
 </body>
  </html>
--- header.php
+++ header.php
@@ -14,5 +14,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
  </head>
- <body              <?php body_class('hello-class'); ?> >
-<?php wp_body_open(); ?>
+ <body                                                                                                  <?php body_class('hello-class'); ?> >
+
+
+<?php if (function_exists('wp_body_open')) {
+        wp_body_open();
+}?>
+
+<div id="page" class="site">
+<header>
+      <?php get_template_part('template-parts/header/nav')?>
+</header>
+<div id="content" class='site-content'>
+
