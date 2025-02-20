4174acd828b9934f21aef436358d1f941e3f6cc3
Nayeem Hassan <nayeemhasan042@gmail.com>
2025-02-20 16:32:24 +0600

    calss17 title tag and custom logo

--- functions.php
+++ functions.php
@@ -9,6 +9,10 @@ if (! defined('SHIMU_DIR_PATH')) {
     define('SHIMU_DIR_PATH', untrailingslashit(get_template_directory()));
 }
 
+if (! defined('SHIMU_DIR_URL')) {
+    define('SHIMU_DIR_URL', untrailingslashit(get_template_directory_uri()));
+}
+
 require_once SHIMU_DIR_PATH . '/inc/helpers/autoloader.php';
 
 function shimu_get_theme_instance()
@@ -20,15 +24,5 @@ shimu_get_theme_instance();
 
 function shimu_enqueue_scripts()
 {
-    // CSS
-    wp_enqueue_style('style', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
-    wp_register_style('main-css', get_template_directory_uri() . '/assets/css/main.css', [], filemtime(get_template_directory() . '/assets/css/main.css'), 'all');
-    wp_enqueue_style('main-css');
-    wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css', [], false, );
 
-    //JS
-    wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/js/main.js', [], filemtime(get_template_directory() . '/assets/js/main.js'), true);
-    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', ['jquery'], false, true);
 }
-
-add_action('wp_enqueue_scripts', 'shimu_enqueue_scripts');
--- inc/classes/class-shimu-theme.php
+++ inc/classes/class-shimu-theme.php
@@ -15,11 +15,26 @@ class SHIMU_THEME
     protected function __construct()
     {
         //load class
-        wp_die('hello');
-        $this->set_hooks();
+        Assets::get_instance();
+        $this->setup_hooks();
     }
-    protected function set_hooks()
+    protected function setup_hooks()
     {
         // actions and filters
+        add_action('after_setup_theme', [$this, 'setup_theme']);
     }
+
+    public function setup_theme()
+    {
+        add_theme_support('title-tag');
+        add_theme_support('custom-logo', [
+            'header-text' => ['site-title', 'site-description'],
+            'height'      => 100,
+            'width'       => 400,
+            'flex-height' => true,
+            'flex-width'  => true,
+        ]);
+
+    }
+
 }
